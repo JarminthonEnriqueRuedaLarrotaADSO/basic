f49e963d0607561f26cc034a37b40ba1cfcafae2
JarminthonEnriqueRuedaLarrotaADSO <pocopoc4158j@gmail.com>
2023-04-09 22:50:58 -0500

    feat: :sparkles: vueBasicoTaller2
    Se llevo a cabo la culminacion del formulario y consultas con vue 3 del segundo taller basico con vue

--- component/component.js
+++ component/component.js
@@ -1,71 +1,133 @@
 export default {
     data() {
         return {
+            listEstudiante:[],
+            listProfesor:[],
+            cons:false,
+            reporte:false,
             select:false,
             selectE:false,
             selectP:false,
             rol:{
                 r1:'Estudiante', r2:'Profesor'
             },
+            // sedes---------------------------------------
             sedes:{
                 s1:'Bucaramanga',
                 s2:'Bogota',
                 s3:'Medellin',
                 s4:'Mexico'
             },
+            direccionSede:null,
+            telSede:null,
+            sede:[],
             // -------------------------------------------
             nombreProfesor: '',
             telefonoProfesor:'',
             teamsProfesor:'',
-            teams:{
+            teamsP:{
                 dia:'',
                 hora:'',
                 salones:''
             },
             email:'',
+            modalidad:'',
             // -------------------------------------------
             nombreEstudiante: '',
-            Lista:[]
-
+            telefonoEstudiante:'',
+            teamsEstudiante:'',
+            teamsE:{
+                dia:'',
+                hora:'',
+                salones:''
+            },
+            asignatura:'',
+            prerrequisito:'',
+            // -------------------------------------------
+            boton:null,
+            radios:false,
+            radios2:''
+            // persona:{
+            //     nombre:'carlos',
+            //     edad:24
+            // }
 
 
 
         }
     },
     methods: {
-        ingresarDatosP() {
-            const profesor ={
-                nombreProfesor: this.nombreProfesor,
-                telefonoProfesor: this.telefonoProfesor,
-                teamsProfesor: this.teamsProfesor,
-                teams: this.teams.dia,
-                teams: this.teams.hora,
-                teams: this.teams.salones
-            }
-            this.Lista.push(profesor)
-            this.nombreProfesor = ''
-            this.telefonoProfesor =''
-            console.log(this.Lista);
-        },
-        ocultar(select){
-            if (this.select == select ) {
+        ocultar(){
+            if (this.select == false ) {
                 this.select = true;
-                console.log(this.select);
             }
-            if (this.select == 'Estudiante') {
-                console.log('hola');
+            else if (this.boton == 'Estudiante') {
                 this.selectE = true;
-                this.selectP = false
-                
-                
+                this.selectP = false;
             }
-            if (this.select == 'Profesor') {
-                console.log('hola12');
+            else if (this.boton == 'Profesor') {
                 this.selectP = true;
-                this.selectE = false
-                
-                
+                this.selectE = false 
+            }
+        },
+        meterDatos(){
+            const dtelSede = {
+                tel:this.telSede,
+                direc:this.direccionSede,
+                lugarSede: this.sedes
+            } 
+            this.sede.push(dtelSede)
+
+            if (this.boton == 'Estudiante') {
+                const datosEstudiante = {
+                    nombreEstudiante: this.nombreEstudiante,
+                    telefonoEstudiante: this.telefonoEstudiante,
+                    teamsEstudiante: this.teamsEstudiante,
+                    dia: this.teamsE.dia,
+                    Hora: this.teamsE.hora,
+                    salones: this.teamsE.salones,
+                    pre : this.prerrequisito,
+                    asig: this.asignatura     
+                } 
+                this.listEstudiante.push(datosEstudiante)
+                console.log(this.listEstudiante); 
+            }
+            else if (this.boton == 'Profesor') {
+                const datosProfesor = {
+                    nombreProfesor: this.nombreProfesor,
+                    telefonoProfesor: this.telefonoProfesor,
+                    teamsProfesor: this.teamsProfesor,
+                    dia: this.teamsP.dia,
+                    Hora: this.teamsP.hora,
+                    salones: this.teamsP.salones,
+                    modalidad: this.modalidad,
+                    email: this.email
+                } 
+                this.listProfesor.push(datosProfesor)
+                console.log(this.listProfesor); 
+            }
+            
+        },
+        mConsulta(){
+            this.cons = !this.cons
+            // if (this.cons == this.cons) {
+            // this.cons= true
+            // }
+        },
+        mReporte (){
+            if (this.reporte == this.reporte) {
+                this.reporte = true
             }
         }
-    }
+// consultas----------------------------------------------
+// consultas con destructing------------------------------
+    },
+    // computed:{
+    //     name(){
+    //         const {nombre} = this.persona
+    //         return nombre
+    //     } @click.prevent="mConsulta"
+    
+
+    // }
 }
